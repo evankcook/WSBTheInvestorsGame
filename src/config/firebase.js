4326cdaff6c65856f08df0de3ec36ae9b797f856
Evan Cook <cook.evank@gmail.com>
2023-01-15 22:46:15 -0600

    complete firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNjEatf5kP3XSjgHgagVWUSPGM9Vs2xZo",
+  authDomain: "wallstreetbetsgame.firebaseapp.com",
+  projectId: "wallstreetbetsgame",
+  storageBucket: "wallstreetbetsgame.appspot.com",
+  messagingSenderId: "1071263267944",
+  appId: "1:1071263267944:web:2d08505b152e014f9d8fdd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
